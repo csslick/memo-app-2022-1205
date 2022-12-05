6ad987492ba410ccab0cbf66a835623e83ae574d
JuHwan <juhwan203@kakao.com>
2022-12-05 11:33:00 +0900

    입력내용추가

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@
 
 // 입력시 실행될 함수
 function handleAdd() {
-  console.log('추가')
+  // memo 입력요소
+  const memoEl = document.getElementById('memo');
+
+  // li(문서객체) 생성
+  const li = document.createElement('li');
+  // p 생성
+  const p = document.createElement('p');
+  console.log(li, p);
+
+  // memo 입력값: value
+  console.log(memoEl.value);
+
+  // p요소에 입력값을 추가
+  p.textContent = memoEl.value;
+  console.log(p)
+  memoEl.value = ''; // 입력 완료 후 내용 비움(초기화)
+
+  // 삭제버튼
+  const delBtn = document.createElement('button');
+  delBtn.textContent = '삭제';
+  
+  // li에 p와 button 추가
+  li.appendChild(p);
+  li.appendChild(delBtn);
+  console.log(li)
+  
 }
 
 
